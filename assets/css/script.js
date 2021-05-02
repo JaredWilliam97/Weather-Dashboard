fd4c67e2cc943a56c9c72cbef26a60e9364db768
JaredWilliam <sutch002@gmail.com>
2021-05-02 15:11:01 -0500

    updated css and js

--- assets/css/script.js
+++ assets/css/script.js
@@ -1,2 +1,13 @@
+var apiKey = "1d6fd810a7af591bd185bc2ab7f866ba";
 
-
+document
+  .getElementById("search-city-button")
+  .addEventListener("click", function () {
+    var city = "";
+    var url =
+      "http://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&appid=" +
+      apiKey +
+      "&units=imperial";
+  });
