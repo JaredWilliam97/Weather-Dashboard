e31a4b457a787529c6812b431c35e12a011e1635
JaredWilliam <sutch002@gmail.com>
2021-05-04 21:24:18 -0500

    modifed html css added different api key and fucntions

--- assets/css/script.js
+++ assets/css/script.js
@@ -1,18 +1,17 @@
-// Created api key
-
-var apiKey = "1d6fd810a7af591bd185bc2ab7f866ba";
-var apiKey2 = ""
-  .getElementById("search-city-button")
-  .addEventListener("click", function () {
-    var city = "";
-    var url =
-      "http://api.openweathermap.org/data/2.5/weather?q=" +
-      city +
-      "&appid=" +
-      apiKey +
-      "&units=imperial";
-  });
-var APIKey = "83fabb81ed82f75df63ff5d8ae957d0c";
+// Attempted this method but had to change 
+// var apiKey = "1d6fd810a7af591bd185bc2ab7f866ba";
+// var apiKey2 = ""
+//   .getElementById("search-city-button")
+//   .addEventListener("click", function () {
+//     var city = "";
+//     var url =
+//       "http://api.openweathermap.org/data/2.5/weather?q=" +
+//       city +
+//       "&appid=" +
+//       apiKey +
+//       "&units=imperial";
+//   });
+var APIKey = "1d6fd810a7af591bd185bc2ab7f866ba";
 var cityArray = [];
 
 function displayCurrentWeather(city) {
@@ -23,5 +22,28 @@ function displayCurrentWeather(city) {
     APIKey +
     "&units=imperial";
 
-  console.log(queryURL);
-
+    $("#search-button").on("click", function (event) {
+      event.preventDefault();
+    
+      var inputCityName = $("#city-input").val().trim();
+      cityArray.push(inputCityName);
+    
+      $(".city").text(inputCityName);
+    
+      var todayDate = $(".today-date");
+      console.log(todayDate);
+    
+      // Should I have added a <br> here?
+      $(todayDate).text("(" + moment().format("MM/DD/YYYY") + ")");
+    
+      var fiveDayText = $("#five-day-text");
+      console.log(fiveDayText);
+      $(fiveDayText).text("3-Hour Forecast: ");
+    
+      // These are the functions in one place
+    
+      displayCurrentWeather(inputCityName);
+      displaySearchedCity(inputCityName);
+      fiveDayForecast(inputCityName);
+      console.log(cityArray);
+    });
